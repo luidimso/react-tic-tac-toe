31ad6ed6df0ac9da71944d7cac3bbdba95579fa1
luidimso <luidi_mso@outlook.com>
2023-11-09 15:56:05 -0300

    Creating the edit and save mode method on PlayerInfo

--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function PlayerInfo({name, symbol}) {
+    const [isEditing, setIsEditing] = useState(false);
+
+    function edit() {
+        setIsEditing(true);
+    }
+
+    function save() {
+        setIsEditing(false);
+    }
+
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {!isEditing && <span className="player-name">{name}</span>}
+              {isEditing && <input type="text" required></input>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            {!isEditing && <button onClick={edit}>Edit</button>}
+            {isEditing && <button onClick={save}>Save</button>}
         </li>
     );
 }
