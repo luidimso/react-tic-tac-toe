ed9cf03462f6bdadadce31cd28057c7c85910d96
luidimso <luidi_mso@outlook.com>
2023-11-09 16:52:02 -0300

    Implementing the method to change the array of tic-tac-toe board value

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,6 +7,19 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleSelectSquare(rowIndex, colIndex, symbol) {
+        setGameBoard((previousGameBoard) => {
+            const updatedBoard = [...previousGameBoard.map(innerArray => [...innerArray])];
+            updatedBoard[rowIndex][colIndex] = symbol;
+
+            console.log(updatedBoard);
+
+            return updatedBoard;
+        })
+    }
+
     return (
         <ol id="game-board">
             {initialGameBoard.map((row, rowIndex) => (
@@ -12,7 +27,7 @@ export default function GameBoard() {
                     <ol>
                         {row.map((playerSymbol, colIndex) => (
                          <li key={colIndex}>
-                            <button>{playerSymbol}</button>
+                            <button onClick={() => handleSelectSquare(rowIndex, colIndex, "X")}>{playerSymbol}</button>
                         </li>
                     ))}
                     </ol>
